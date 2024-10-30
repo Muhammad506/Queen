ad991ed3ff4993387afa91236fde7565cae3f579
Muhammad506 <muhammadumersaleem408@gmail.com>
2024-10-30 15:53:51 +0500

    ok

--- app/page.js
+++ app/page.js
@@ -4,16 +4,13 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "@/components/Home/Home";
 import AdmissionPage from "@/components/Admission/AdmissionPage";
 
-
 const App = () => {
   return (
     <Router>
-    
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/admission" element={<AdmissionPage />} />
       </Routes>
- 
     </Router>
   );
 };
